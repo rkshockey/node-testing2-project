f3495af12958ee5058fa93818dbda1f3cf8799dc
rkshockey <rkshockey@gmail.com>
2021-09-16 14:45:50 -0400

    Corrected error

--- data/migrations/20210916182337_users-table.js
+++ data/migrations/20210916182337_users-table.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex) {
-  
+    knex.schema.createTable('users', table => {
+        table.increments('id');
+        table.string('username').unique().notNullable();
+        table.string('password').notNullable()
+    });
 };
 
 exports.down = function(knex) {
-  
+  knex.schema.dropTableIfExists('users');
 };
